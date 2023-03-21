c34dbf5e130279338992b13c23e6728416f023bf
anjaliruth <ruthanjali0211@gmail.com>
2023-03-21 20:26:38 +0800

    Task 1

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const button = document.querySelector("#click-me");
 
 function handleClick(event) {
+  let shiftKey = event.shiftKey
   // If clicked, console log you clicked me!
+  console.log('You clicked me!')
+  console.log(event);
+// 👉 Add code so that if the `shiftKey` property of the event is `true` 
+// (in other words, if the user holds the shift key down while they click the button), 
+//then change the text in the button to be `"NAILED IT!!"`.
+
+//if shift key is true
+// change text to nailed it
+
+if (shiftKey) {
+  console.log("Shift Pressed", event);
+}//have that button when clicked, change text to nailed it. 
+  return (button.textContent = "NAILED IT!")
+
+  
   // If shift key pressed, change the inner text of the button to be NAILED IT
 }
 
